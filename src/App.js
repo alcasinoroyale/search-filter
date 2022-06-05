e6ef396463a94187fe212b4a041bd6ac891ca51f
alcasinoroyale <alexzdatny@gmail.com>
2022-06-05 02:30:13 -0400

    Add Mock data and div for search list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import JSONDATA from './names.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Search Filter
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Search..." />
+      {JSONDATA.map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
